3ed5c0e83be93d7fa1906c60a994307706da4b8d
Nishit Mohanan <nishitmohanan@gmail.com>
2019-01-28 23:18:50 +0530

    Added countdown component and helper functions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./css/App.css";
 import "./components/SiteInput";
 import SiteInput from "./components/SiteInput";
+import Countdown from "react-countdown-now";
 class App extends Component {
   state = {
     minutes: 5,
@@ -13,6 +14,32 @@ class App extends Component {
       minutes: time
     });
   };
+
+  startTimer = () => {
+    this.countdownRef.current.api.start();
+    this.countdownRef2.current.api.start();
+
+  };
+
+  customRenderer = ({ hours, minutes, seconds, completed }) => {
+    if (!completed) {
+      let minutesString = minutes;
+      let secondsString = seconds;
+
+      if(minutes<10){
+        minutesString = `0${minutes}`
+      }
+      if(seconds<10){
+        secondsString = `0${seconds}`
+      }
+      document.title = `${minutesString}:${secondsString}`;
+    }
+    return <span />;
+  };
+
+  countdownRef = React.createRef();
+  countdownRef2 = React.createRef();
+
   render() {
     return (
       <div className="App">
@@ -20,7 +47,24 @@ class App extends Component {
           <span>Temp Tabs</span>
         </div>
         <div className="site-input">
-          <SiteInput setTime={this.setTime} minutes={this.state.minutes} />
+          <SiteInput
+            setTime={this.setTime}
+            minutes={this.state.minutes}
+            startTimer={this.startTimer}
+          />
+        </div>
+        <div className="timer">
+          <Countdown
+            ref={this.countdownRef}
+            autoStart={false}
+            date={Date.now() + this.state.minutes * 60000}
+          />
+          <Countdown
+            ref={this.countdownRef2}
+            autoStart={false}
+            date={Date.now() + this.state.minutes * 60000}
+            renderer={this.customRenderer}
+          />
         </div>
       </div>
     );
