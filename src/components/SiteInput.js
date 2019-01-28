24182fb72f1728c2c35bcfc2cdb24c0a9cb674de
Nishit Mohanan <nishitmohanan@gmail.com>
2019-01-28 23:19:24 +0530

    Added datalist + code formating

--- src/components/SiteInput.js
+++ src/components/SiteInput.js
@@ -12,7 +12,7 @@ class SiteInput extends React.Component {
     // from the timeselector componenent
     // then starts the timer display
     e.preventDefault();
-    console.log(this.urlInput.current.value);
+    this.props.startTimer();
     var win = window.open(this.urlInput.current.value, "_blank");
     win.focus();
     setTimeout(function() {
@@ -25,19 +25,47 @@ class SiteInput extends React.Component {
     // access to e.target here
     console.log(data);
     this.props.setTime(data);
-};
+  };
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <input type="url" placeholder="Enter Url" ref={this.urlInput} />
+        <input type="url" placeholder="Enter Url" ref={this.urlInput} list="defaultURLs"/>
+
         <div className="time-selector">
-          <span onClick={((e) => this.handleClick(e, 5))} className={this.props.minutes=== 5 ? 'selected':''}>5 Minutes</span>
-          <span onClick={((e) => this.handleClick(e, 10))} className={this.props.minutes=== 10 ? 'selected':''}>10 Minutes</span>
-          <span onClick={((e) => this.handleClick(e, 15))} className={this.props.minutes=== 15 ? 'selected':''}>15 Minutes</span>
-          <span onClick={((e) => this.handleClick(e, 30))} className={this.props.minutes=== 30 ? 'selected':''}>30 Minutes</span>
+          <span
+            onClick={e => this.handleClick(e, 5)}
+            className={this.props.minutes === 5 ? "selected" : ""}
+          >
+            5 Minutes
+          </span>
+          <span
+            onClick={e => this.handleClick(e, 10)}
+            className={this.props.minutes === 10 ? "selected" : ""}
+          >
+            10 Minutes
+          </span>
+          <span
+            onClick={e => this.handleClick(e, 15)}
+            className={this.props.minutes === 15 ? "selected" : ""}
+          >
+            15 Minutes
+          </span>
+          <span
+            onClick={e => this.handleClick(e, 30)}
+            className={this.props.minutes === 30 ? "selected" : ""}
+          >
+            30 Minutes
+          </span>
         </div>
         <button>{`Open Url for ${this.props.minutes} minutes`}</button>
+        <datalist id="defaultURLs">
+          <option value="https://developer.mozilla.org/" />
+          <option value="https://reddit.com/" />
+          <option value="https://youtube.com/" />
+          <option value="https://google.com/" />
+          <option value="http://hckr.netlify.com/" />
+        </datalist>
       </form>
     );
   }
